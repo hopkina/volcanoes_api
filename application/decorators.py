e2a6e9e4dca87efb759b7ae47592ad974b94268c
djsp <djspoulter@gmail.com>
2019-06-25 22:18:20 +0100

    Removed @wraps decorator as it confuses Flask.

--- application/decorators.py
+++ application/decorators.py
@@ -1,12 +1,10 @@
-from functools import wraps
-
 from application.formatters import as_json
 
 
 def as_feature_collection(func):
-    @wraps
     def wrapped_view(*args, **kwargs):
         data = func(*args, **kwargs)
         return as_json(data)
 
+    wrapped_view.__name__ = func.__name__
     return wrapped_view
